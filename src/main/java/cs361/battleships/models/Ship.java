c0ac65979577924a45a583672ea1be800a7911b4
joshuasfisher <fisherj2@oregonstate.edu>
2018-10-09 17:44:20 -0700

    update master branch based on slack announcement message

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -3,11 +3,16 @@ package cs361.battleships.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Ship {
 
 	@JsonProperty private List<Square> occupiedSquares;
-	
+
+	public Ship() {
+		occupiedSquares = new ArrayList<>();
+	}
+
 	public Ship(String kind) {
 		//TODO implement
 	}
