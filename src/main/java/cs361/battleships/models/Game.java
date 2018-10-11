5f63fee95d29ea44d5892319b7bbfeed3196468d
dillanea <dillanea@oregonstate.edu>
2018-10-10 19:26:52 -0700

    Code for Randomizers in Game file

--- src/main/java/cs361/battleships/models/Game.java
+++ src/main/java/cs361/battleships/models/Game.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static cs361.battleships.models.AttackStatus.*;
 
@@ -12,6 +13,11 @@ public class Game {
     @JsonProperty private Board playersBoard = new Board();
     @JsonProperty private Board opponentsBoard = new Board();
 
+    private static final String alphabet = "ABCDEFGHIJ";
+
+    public String getAlphabet(){
+        return alphabet;
+    }
     /*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
@@ -50,17 +56,25 @@ public class Game {
     }
 
     private char randCol() {
-        // TODO implement
-        return 'X';
+        //This will select a random character between A - J
+        Random r = new Random();
+
+        //Declare a string that contains possible choices
+        //From this string, randomly select an index that will be returned
+        String alphabet = "ABCDEFGHIJ";
+        return this.alphabet.charAt(r.nextInt(alphabet.length()));
     }
 
     private int randRow() {
-        // TODO implement
-        return 0;
+        //This will return a random number between 1 - 10
+        Random r = new Random();
+        return (r.nextInt(10) + 1);
     }
 
     private boolean randVertical() {
-        // TODO implement
-        return false;
+        //This will return true or false depending if the random
+        //integer from 0-1 is 0
+        Random r = new Random();
+        return (r.nextInt(2) == 0);
     }
 }
