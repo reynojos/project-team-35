55bb5cade78253a95d49b967d499565c265a3654
Devin Swift <dswift2423@gmail.com>
2018-10-11 23:12:19 -0700

    Add Square testing

--- src/test/java/cs361/battleships/models/SquareTest.java
+++ src/test/java/cs361/battleships/models/SquareTest.java
@@ -7,4 +7,20 @@ import static org.junit.Assert.assertTrue;
 
 public class SquareTest {
 
+    @Test
+    public void testNewSquare() {
+
+        // Test: Default Constructor, setRow, setColumn, getRow, getColumn
+        Square square1 = new Square();
+
+        square1.setRow(1);
+        square1.setColumn('A');
+        assertTrue(square1.getRow() == 1);
+        assertTrue(square1.getColumn() == 'A');
+
+        // Test: Override constructor, getRow, getColumn
+        Square square = new Square(1, 'A');
+        assertTrue(square.getRow() == 1);
+        assertTrue(square.getColumn() == 'A');
+    }
 }
